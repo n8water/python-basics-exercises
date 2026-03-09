3b78e4b1e3601e6b1cb55f6af83bc656e765eb25
Felder Sabrina <Sabrina.Felder@Doppelmayr.com>
2026-03-09 11:47:31 +0100

    solved line-up.py from exercism

--- exercism.org/line-up.py
+++ exercism.org/line-up.py
@@ -1,5 +1,20 @@
 """ Module to inform customers about their number in line of customers """
 def line_up(name, number):
     """ Return a string telling customer rank in line """
+    if number % 10 == 1 and not number % 100 == 11:            
+        suffix = "st"
+    elif number % 10 == 2 and not number % 100 == 12:
+        suffix = "nd"
+    elif number % 10 == 3 and not number % 100 == 13:
+        suffix = "rd"
+    else:
+        suffix = "th"
+
+    return f"{name}, you are the {number}{suffix} customer we serve today. Thank you!"
     
-    pass
+
+print(line_up("Mary", 1))
+print(line_up("John", 12))
+print(line_up("Dahir", 9))
+
+
